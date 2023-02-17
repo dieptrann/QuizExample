542e1cea6c2f6650d27799b83cb4e6ae655dab2a
thucnuu <nu.dong@nfq.com>
2023-02-17 14:00:02 +0700

    component: Welcome, Quiz

---
+++ src/components/QuizCard.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+
+
+</script>
+
+<template>
+    <div class="wrapper">
+ffffffff
+    </div>
+</template>
+
+
+
+<style scoped>
+.wrapper{
+    height: 500px;
+    width: 800px;
+    background-color: #fff;
+    border-radius: 10px;
+
+}
+</style>
